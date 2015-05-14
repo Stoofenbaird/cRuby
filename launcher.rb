44ab3d07c33e1373fcfdaffa638f62d5e82d12ac
Stoofenbaird <mim@stoofenbaird.com>
2015-05-14 14:01:20 -0500

    Added step 3

--- launcher.rb
+++ launcher.rb
@@ -5,9 +5,13 @@
 #Example application to demonstrate some basic Ruby features
 #This code loads a given file into an associated application
 
-# Create a variable
+# Step 1: Set up the file and Create a variable
 class Launcher
+	# Step 3: Adding behavior using def keyword, initiailize method and an argument
+	def initialize app_map
+		@app_map = app_map
 end
 
-# Create an instance of the class Launcher
+# Step 2: Create an object instance by invoking new on the Launcher class.
 launcher = Launcher.new
+
