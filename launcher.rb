f6c7e7a6e873875b8ef67497a8fef96073bf08a5
Stoofenbaird <mim@stoofenbaird.com>
2015-05-14 12:56:24 -0500

    Step 1 in tutorial

---
+++ launcher.rb
@@ -0,0 +1,9 @@
+# Tutorial from http://neurogami.com/content/neurogami-10_minutes_to_your_first_Ruby_app/
+
+#!/usr/bin/env ruby 
+
+#Example application to demonstrate some basic Ruby features
+#This code loads a given file into an associated application
+
+class Launcher
+end
