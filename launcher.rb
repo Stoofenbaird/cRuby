bb403fb0c12f99d603926c80dc5923a82eb23e6e
Stoofenbaird <mim@stoofenbaird.com>
2015-05-14 14:24:03 -0500

    Added step 4

--- launcher.rb
+++ launcher.rb
@@ -13,5 +13,28 @@ class Launcher
 end
 
 # Step 2: Create an object instance by invoking new on the Launcher class.
-launcher = Launcher.new
+# Step 2a: This line is modified later in the program.  Comment out for now.
+# launcher = Launcher.new
+
+# Step 4: Execute the given file using the associated app. Take a file name and
+# pass it to select_app  
+def run file_name
+	application = select_app file_name
+	# system uses Ruby's system method to invoke the application, passing it the file name
+	system "#{application} #{file_name}"
+end
+
+# Given a file, look up the matching application
+def select_app file_name
+	ftype = file_type file_name
+	@app_map[ ftype ]
+end
+
+# Return the part of the file name string after the last '.', a normalized file ext.
+# gsub is global substitute
+def file_type file_name
+	File.extname( file_name ).gsub( /^\./, '' ).downcase
+end
+
+end
 
