5fa0e6644374b2674f93e7d7b5ba1c3a37115bd0
Stoofenbaird <mim@stoofenbaird.com>
2015-05-14 14:35:20 -0500

    Added Step 5

--- launcher.rb
+++ launcher.rb
@@ -10,31 +10,50 @@ class Launcher
 	# Step 3: Adding behavior using def keyword, initiailize method and an argument
 	def initialize app_map
 		@app_map = app_map
+	end
+
+	# Step 2: Create an object instance by invoking new on the Launcher class.
+	# Step 2a: This line is modified later in the program.  Comment out for now.
+	# launcher = Launcher.new
+
+	# Step 4: Execute the given file using the associated app. Take a file name and
+	# pass it to select_app  
+	def run file_name
+		application = select_app file_name
+		# system uses Ruby's system method to invoke the application, passing it the file name
+		system "#{application} #{file_name}"
+	end
+
+	# Given a file, look up the matching application
+	def select_app file_name
+		ftype = file_type file_name
+		@app_map[ ftype ]
+	end
+
+	# Return the part of the file name string after the last '.', a normalized file ext.
+	# gsub is global substitute
+	def file_type file_name
+		File.extname( file_name ).gsub( /^\./, '' ).downcase
+	end
+end
+# Step 5: Create an instance of Launcher and use it to run an application
+def help
+	print "You must pass in the path to the file to launch.
+	Usage; #{__FILE__} target_file"
 end
 
-# Step 2: Create an object instance by invoking new on the Launcher class.
-# Step 2a: This line is modified later in the program.  Comment out for now.
-# launcher = Launcher.new
-
-# Step 4: Execute the given file using the associated app. Take a file name and
-# pass it to select_app  
-def run file_name
-	application = select_app file_name
-	# system uses Ruby's system method to invoke the application, passing it the file name
-	system "#{application} #{file_name}"
+unless ARGV.size > 0
+	help
+	exit
 end
 
-# Given a file, look up the matching application
-def select_app file_name
-	ftype = file_type file_name
-	@app_map[ ftype ]
-end
+app_map = {
+	'html' => 'chromium-browser -new-window --enable-plugins  --allow-outdated-plugins ',
+	'rb' => 'gvim',
+	'jpg' => 'gimp'
+}
 
-# Return the part of the file name string after the last '.', a normalized file ext.
-# gsub is global substitute
-def file_type file_name
-	File.extname( file_name ).gsub( /^\./, '' ).downcase
-end
-
-end
+l = Launcher.new app_map
+target = ARGV.join ' '
+l.run target 
 
