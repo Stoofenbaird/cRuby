d1a1ffc81f1f57aaa58006cb6f18e2f3e721a2ad
Stoofenbaird <mim@stoofenbaird.com>
2015-05-21 11:06:06 -0500

    Added fill_in Title block lines 9-13

--- odot/spec/features/todo_lists/create_spec.rb
+++ odot/spec/features/todo_lists/create_spec.rb
@@ -5,5 +5,11 @@ describe "Creating todo lists" do
 		visit "/todo_lists"
 		click_link "New Todo list"
 		expect(page).to have_content("New Todo List")
+
+		fill_in "Title", with: "My todo list"
+		fill_in "Description", with: "This is what I'm doing today."
+		click_button "Create Todo list"
+
+		expect(page).to have_content("My todo list")
 	end
 end
