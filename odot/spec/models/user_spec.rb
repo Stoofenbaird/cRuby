26fd0812872ed8e62a64f6ffcf643f45dcb31b0c
Stoofenbaird <mim@stoofenbaird.com>
2015-06-03 16:26:28 -0500

    added mixed case email, finished downcase

--- odot/spec/models/user_spec.rb
+++ odot/spec/models/user_spec.rb
@@ -23,13 +23,19 @@ describe User do
       expect(user).to validate_uniqueness_of(:email)
     end
 
+    it "requires a unique email (case insensitive)" do
+      user.email = "JASON@TEAMTREEHOUSE.COM"
+      expect(user).to validate_uniqueness_of(:email)
+    end
+
   end
 
   describe "#downcase_email" do
     it "makes the email attribute lower case" do
       user = User.new(valid_attributes.merge(email: "JASON@TEAMTREEHOUSE.COM"))
-      user.downcase_email
-      expect(user.email).to eq("jason@teamtreehouse.com")
+      expect{ user.downcase_email }.to change{ user.email}.
+      from("JASON@TEAMTREEHOUSE.COM").
+      to("jason@teamtreehouse.com")
     end
 
   it "downcases an email before saving" do
