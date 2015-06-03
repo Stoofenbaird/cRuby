4809be3850d32f3c793b3d9458e9da8fa929b570
Stoofenbaird <mim@stoofenbaird.com>
2015-06-03 15:35:12 -0500

    Videos 1.2 and 1.3 setup and initial user validation

---
+++ odot/spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe User do
+  let(:valid_attributes) {
+    {
+      first_name: "Jason",
+      last_name: "Seifer",
+      email: "jason@teamtreehouse.com"
+    }
+  }
+  context "validations" do
+    let(:user) { User.new(valid_attributes) }
+    it "requires an email" do
+      expect(user).to validate_presence_of(:email)
+    end
+  end
+  
+end
