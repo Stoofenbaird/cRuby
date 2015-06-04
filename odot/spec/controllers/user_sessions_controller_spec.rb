af2ba76f560aae06dfb4cd0c0f1b5f33f1fa7419
Stoofenbaird <mim@stoofenbaird.com>
2015-06-04 16:55:15 -0500

    Added user authentication

--- odot/spec/controllers/user_sessions_controller_spec.rb
+++ odot/spec/controllers/user_sessions_controller_spec.rb
@@ -15,10 +15,26 @@ describe UserSessionsController do
   end
 
   describe "POST 'create'" do
-    it "redirects to the todo list path" do
-      post :create, email: "jason#@teamtreehouse.com", password: "treehouse1"
-      expect(response).to be_redirect
-      expect(response).to redirect_to(todo_lists_path)
+    context "with correct credentials" do
+      let!(:user) { User.create(first_name: "Jason", last_name: "Seifer", email: "jason@teamtreehouse.com", password: "treehouse1", password_confirmation: "treehouse1")}
+ 
+      it "redirects to the todo list path" do
+        post :create, email: "jason@teamtreehouse.com", password: "treehouse1"
+        expect(response).to be_redirect
+        expect(response).to redirect_to(todo_lists_path)
+      end
+
+      it "finds the user" do
+        User.stub(:find_by).and_return(user)
+        expect(user).to receive(:authenticate)
+        post :create, email: "jason@teamtreehouse.com", password: "treehouse1"
+      end
+
+      it "authenticates the user" do
+        User.stub(:find_by).and_return(user)
+        expect(user).to receive(:authenticate)
+        post :create, email: "jason@teamtreehouse.com", password: "treehouse1"
+      end
     end
   end
 
