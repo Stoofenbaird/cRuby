e5855fc603324235180e8e1e6dc3b41fecbbcf1a
Stoofenbaird <mim@stoofenbaird.com>
2015-06-17 14:30:32 -0500

    Added user profile tests, modified user yaml

--- treebook/app/models/user.rb
+++ treebook/app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   validates :first_name, presence: true
 
   validates :last_name, presence: true
 
-  validates :profile_name, presence: true
+  validates :profile_name, presence: true, uniqueness: true
 end
--- treebook/test/models/user_test.rb
+++ treebook/test/models/user_test.rb
@@ -18,4 +18,13 @@ class UserTest < ActiveSupport::TestCase
     assert !user.save
     assert !user.errors[:profile_name].empty?
   end
+
+  test "a user should have a unique profile name" do 
+    user = User.new
+    user.profile_name = users(:jason).profile_name
+    
+    assert !user.save
+    puts user.errors.inspect
+    assert !user.errors[:profile_name].empty?
+  end
 end
