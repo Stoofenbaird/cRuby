f62192e0b7f0c78b4f99e60d53e8eb774e0fe983
Stoofenbaird <mim@stoofenbaird.com>
2015-06-18 16:12:13 -0500

    Added status validation and tests

--- treebook/app/models/status.rb
+++ treebook/app/models/status.rb
@@ -1,2 +1,7 @@
 class Status < ActiveRecord::Base
+
+  validates :content, presence: true,
+                      length: { minimum: 2 }
+
+  validates :user_id, presence: true                    
 end
